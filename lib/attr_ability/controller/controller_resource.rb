0a60f1d4fec412d047058b9e56aeebc3e9752b13
Alexander Danilenko <alexander@danilenko.org>
2012-06-11 23:04:07 +0400

    When cancan is building new resource assign defaults first then params

--- lib/attr_ability/controller/controller_resource.rb
+++ lib/attr_ability/controller/controller_resource.rb
@@ -3,12 +3,20 @@ require 'cancan'
 module AttrAbility
   module Controller
     class ControllerResource < CanCan::ControllerResource
-      def resource_base
+      def load_resource_instance
         super.as(current_ability)
       end
 
-      def load_resource_instance
-        super.as(current_ability)
+      protected
+
+      def build_resource
+        resource = resource_base.new.as(current_ability)
+        resource.send("#{parent_name}=", parent_resource) if @options[:singleton] && parent_resource
+        initial_attributes.each do |attr_name, value|
+          resource.send("#{attr_name}=", value)
+        end
+        resource.attributes = @params[name] if @params[name].present?
+        resource
       end
     end
   end
